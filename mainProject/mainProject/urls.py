700de3051e56f4c1aa48c982492447aa18924da8
TOMASZ\tomas <tomasz1lesniak@gmail.com>
2026-04-14 19:44:26 +0200

    no message

--- mainProject/mainProject/urls.py
+++ mainProject/mainProject/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from strawberry.django.views import AsyncGraphQLView
 from TripApp.graphql.schema import schema
+from django.http import HttpResponse
+
+def ping(request):
+    return HttpResponse("pong", content_type="text/plain")
 
 urlpatterns = [
     path("graphql/", csrf_exempt(AsyncGraphQLView.as_view(schema=schema))),
+    path("ping/", ping, name="ping")
 ]
